b8698c026532358edb7991fa8e61ccba28d61cfe
Administrator <Administrator@DESKTOP-95RB6IG>
2023-12-11 17:47:14 +0100

    Aggiungere i file di progetto.

---
+++ RequestingData/Program.cs
@@ -0,0 +1,16 @@
+﻿var httpClient = new HttpClient();
+httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+httpClient.DefaultRequestHeaders.Add("User-Agent", "HttpRequesModule");
+
+Console.WriteLine("Inserisci Url:\n");
+
+try
+{
+    var response = await httpClient.GetAsync(Console.ReadLine());
+    Console.WriteLine($"Status: {(int)response.StatusCode} - {response.StatusCode}");
+    Console.WriteLine(await response.Content.ReadAsStringAsync());
+}
+catch(HttpRequestException ex)
+{
+    Console.WriteLine($"Error Bad Request {ex.Message}" );
+}
